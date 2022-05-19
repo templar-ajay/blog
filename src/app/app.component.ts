d780a6a129bad966f8d07cba8c95e77048e6904e
templar-command0 <ajay.kumar@talentelgia.in>
2022-05-19 10:48:23 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,61 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'blog';
+  blogData: { title: string; content: string; dikhanaHH: boolean }[] = [];
+  ix: number;
+
+  onSaveClick(blogTitle: HTMLInputElement, blogContent: HTMLInputElement) {
+    if (blogContent.value !== '') {
+      if (this.ix >= 0) {
+        this.blogData[this.ix].title = blogTitle.value;
+        this.blogData[this.ix].content = blogContent.value;
+        this.blogData[this.ix].dikhanaHH = true;
+      } else {
+        this.blogData.push({
+          title: blogTitle.value,
+          content: blogContent.value,
+          dikhanaHH: true,
+        });
+      }
+    }
+
+    blogTitle.value = '';
+    blogContent.value = '';
+    this.ix = -1;
+  }
+
+  onDeleteClick(i: number) {
+    this.blogData.splice(i, 1);
+  }
+  onEditClick(
+    i: number,
+    blogTitle: HTMLInputElement,
+    blogContent: HTMLInputElement
+  ) {
+    blogTitle.value = this.blogData[i].title;
+    blogContent.value = this.blogData[i].content;
+    this.ix = i;
+  }
+
+  onInputChange(searched: HTMLInputElement) {
+    console.log('oninputWorking');
+    if (searched.value !== '') {
+      console.log('if serached.value working');
+      for (let index in this.blogData) {
+        console.log('for let working');
+        if (this.blogData[index].title.search(searched.value) >= 0) {
+          console.log('if working');
+          this.blogData[index].dikhanaHH = true;
+        } else this.blogData[index].dikhanaHH = false;
+        console.log('else working');
+      }
+    } else {
+      for (let index in this.blogData) {
+        this.blogData[index].dikhanaHH = true;
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  blogElements = [{ title: 'String', content: 'String' }];
+
+  onBlogAdded(blogData: { title: string; content: string }) {
+    this.blogElements.push({
+      title: blogData.title,
+      content: blogData.content,
+    });
+  }
+}
